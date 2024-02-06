23d6594a70c938b1893645d73bda6de97d1d41bf
Kunal Shrivastava <kush0221@netcracker.com>
2024-02-06 19:33:21 +0530

    first commit

--- Python/Module1-Python Basics/TO_DO_APP/pythonProject/Day2.py
+++
@@ -1,7 +0,0 @@
-user_prompt = "Enter a ToDo"
-todo = ""
-todos = []
-while True:
-    todo = input(user_prompt)
-    todos.append(todo.capitalize())
-    print(todos)
--- Python/Module1-Python Basics/TO_DO_APP/pythonProject/bonus1.py
+++
@@ -1,3 +0,0 @@
-title = input('enter a title')
-length = len(title)
-print("the length of the title is ", length)
